5b6c55c9a2f19bcd2773927396c5cb9163c2c10e
Dennis Josefsson <dennis.josefsson@gmail.com>
2024-11-11 18:37:51 +0100

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import './index.css'
+
+export default function Index() {
+  return (
+    <View className="bg-slate-700 h-screen">
+      <Text className="font-bold text-blue-50">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  )
+}
