8f412226b4daaa2fd1b798c88370aabf66773664
Dennis Josefsson <dennis.josefsson@gmail.com>
2024-11-19 08:59:12 +0100

    Removing files

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,20 @@
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import './index.css'
 
 export default function Index() {
   return (
-    <View className="bg-slate-700 h-screen">
-      <Text className="font-bold text-blue-50">
-        Edit app/index.tsx to edit this screen.
+    <View className="bg-slate-900 h-screen flex flex-col">
+      <Text className="font-bold text-slate-100 text-4xl p-4">
+        Hello, world!
       </Text>
+      <FlatList
+        className="p-4"
+        data={['Hello, ', 'World!']}
+        renderItem={({ item }) => (
+          <Text className="text-lg text-slate-100">{item}</Text>
+        )}
+        keyExtractor={(item) => item}
+      />
     </View>
   )
 }
